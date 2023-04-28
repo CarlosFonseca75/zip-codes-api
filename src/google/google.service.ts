07ade81def6b564ba1af08da755cdb268b769984
Carlos Fonseca <cardfonseca07@gmail.com>
2023-04-27 21:45:20 -0600

    Updating cookie.

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -32,7 +32,7 @@ export class GoogleService {
 
       res.cookie('access_token', accessToken, {
         httpOnly: true,
-        sameSite: false,
+        sameSite: "none",
         secure: true,
         expires: expiration,
       });
@@ -43,7 +43,7 @@ export class GoogleService {
 
       res.cookie('user_data', userData, {
         httpOnly: false,
-        sameSite: false,
+        sameSite: "none",
         secure: true,
         expires: expiration,
       });
